60ca807be0cd5964e8ea18021abb9fda783333dd
stachu3478 <stachu3478@o2.pl>
2020-12-18 09:29:56 +0100

    Last changes

---
+++ src/main/java/com/sample/Response.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+public class Response {
+	public String keyword;
+	public String content;
+
+	public Response(String keyword1, String content1) {
+		keyword = keyword1;
+		content = content1;
+	}
+
+}
